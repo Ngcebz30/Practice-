51580b0275d4e81d383e1e7bccf8b5ab9bc8433b
Nongcebo <nssbmcnamarabold@yahoo.com>
2023-03-10 14:46:49 -0800

    Hello-World.c

---
+++ C-Programming/Hello-World.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* main - My 1st C Program
+ * called Hello World
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("Hello World\n");
+	printf("How are you?\n");
+	printf("I'm fine and yourself?\n");
+	printf("I'm great,thank you.\n");
+
+	return (0);
+}
