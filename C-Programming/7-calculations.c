185113704d7e1e91d9803eea45d8d1e572d1be9a
Nongcebo <nssbmcnamarabold@yahoo.com>
2023-03-11 10:41:02 -0800

    7-calculations.c

---
+++ C-Programming/7-calculations.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/* main - Program that does calcilations
+ * Return: 0
+ */
+
+int main(void)
+{
+	// Modulus Calculation here 
+	
+	int a = 19;
+	int b = 3;
+	int c = a % b;
+
+
+	printf("%d\n", a);
+	printf("%d\n", b);
+	printf("The answer is: %d \n", c);
+
+
+	// Multiplication Calculation here 
+	
+	float d = 28.32;
+	float e = 2.5;
+	double f = d * e;
+
+
+	printf("%f\n", d);
+	printf("%f\n", e);
+	printf("The product of f: %lf \n", f);
+
+	// Additon calculation with increment
+	
+	int g = 40;
+	int h = 2;
+	int j = g + j;
+
+	printf("%d\n", g);
+	printf("%d\n", h);
+	printf("The sum: %d \n", j);
+
+	return (0);
+}
