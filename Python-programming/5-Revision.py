4c20d72d876884e20525df6402d19eb85c69d281
Nongcebo <nssbmcnamarabold@yahoo.com>
2023-03-13 18:03:14 -0700

    Revision

---
+++ Python-programming/5-Revision.py
@@ -0,0 +1,169 @@
+# Hello World Program
+
+print("Hello World\n")
+
+print("\n\n\n")
+
+
+
+
+# Just Print Variables Onscreen
+
+print("Just Print Variables Onscreen\n")
+
+a = 10
+b = 20.25
+c = "thirty"
+d = "true"
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+print("\n\n\n")
+
+
+
+
+# Arithmetic Calculations
+
+print("Arithmetic Calculations\n")
+
+a, b, c, d = 50, 5, 8, 2
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(c % d)
+print(c // d)
+print(c ** d)
+
+print("\n\n\n")
+
+
+
+
+
+
+
+# Boolean datavalues in Logical expressions 
+
+print("Boolean datavalues in Logical expressions\n")
+
+a = 'true' and 'false' and 'true' 
+A = 'true' and 'true' and 'true'
+
+b = 'true' or 'false' or 'true'
+B = 'false' or 'false' or 'false'
+
+c = not 'false'
+C = not 'true'
+
+a1 = 'true' and 'false' or 'true' 
+a2 = 'true' or 'true' and 'false' or not 'false' 
+
+print(a)
+print(A)
+print(b)
+print(B)
+print(c)
+print(C)
+print(a1)
+print(a2)
+
+print("\n\n\n")
+
+
+
+
+# Same variable equate to itself
+
+print("Same variable equate to itself\n")
+
+a = 40 
+a += 69
+
+print(a)
+
+
+b = 56
+b -= 21
+
+print(b)
+
+print("\n\n\n")
+
+
+
+
+
+# Strings with + and * calculations
+
+print("Strings with + and ** calculations\n")
+
+a = "blue" + "sky"
+
+print(a)
+
+b = "blue" * 5
+
+print(b)
+
+print("\n\n\n")
+
+
+
+
+
+
+
+# Disguised Boolean 
+
+print("Disguised Boolean\n")
+
+a = 1 and 0
+
+print(a)
+
+b = 0 or 1
+
+print(b)
+
+print("\n\n\n")
+
+
+
+
+
+
+# a few built in functions 
+
+print("a few built in functions\n")
+
+a = "DancingPandaBears"
+
+print(a.startswith("Dan"))
+print(a.startswith("cin"))
+
+print(a.endswith("s"))
+print(a.endswith("b"))
+
+print(len(a))
+
+print("\n\n\n")
+
+
+
+
+
+
+
+# Escape sequence 
+
+print("Escape Sequence\n")
+
+print("Hello\nHow are you today?\nI'm goodl\b\nThat's great to hear\t Oh,really?")
+
+
